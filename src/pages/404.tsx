9505d33da08869fb0b6c2381acf9a776269f3d34
RedWilly <thompsonau023@gmail.com>
2024-09-10 15:28:16 +0000

    custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,13 +9,13 @@ const Custom404: React.FC = () => {
       <Layout>
         <SEO 
           title="404 - Not Found | bondle"
-          description="The page you're looking for doesn't exist or is still indexing."
+          description="The page you're looking for doesn&apos;t exist or is still indexing."
         />
         <div className="flex flex-col items-center justify-start px-4 text-center pt-20 sm:pt-32 pb-10">
           <Ban className="w-16 h-16 sm:w-20 sm:h-20 text-red-500 mb-3" />
           <h1 className="text-xl sm:text-2xl font-bold text-blue-400 mb-4">Not Found</h1>
           <p className="text-[10px] sm:text-xs text-gray-300 mb-5 max-w-xs sm:max-w-sm">
-            Wait and try again later. It seems like what you are looking for doesn't exist or is still indexing.
+            Wait and try again later. It seems like what you are looking for doesn&apos;t exist or is still indexing.
           </p>
           <Link href="/" passHref>
             <button className="bg-blue-500 hover:bg-blue-600 text-white text-xs sm:text-sm font-bold py-2 px-3 rounded transition duration-300">
